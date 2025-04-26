06bdc9e3bcabe8314dbfe787764dc162f278c522
Chris <chrisbreuer93@gmail.com>
2025-04-26 00:22:24 -0700

    chore: resolve bun issue
    unsure why this was caused

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,24 @@
-import type { UserConfig } from '@unocss/core'
 import type { BunPlugin } from 'bun'
 import { loadConfig } from '@unocss/config'
 import { createGenerator } from '@unocss/core'
 
-export function plugin(userConfig?: UserConfig): BunPlugin {
-  return {
-    name: 'bun-plugin-unocss',
-    async setup(build) {
-      // Load configuration
-      const config = userConfig || (await loadConfig()).config
-      const generator = await createGenerator(config)
+export const plugin: BunPlugin = {
+  name: 'bun-plugin-unocss',
+  async setup(build) {
+    // Load configuration or use preset-uno if no config
+    const config = await loadConfig()
+    const generator = await createGenerator(config.config)
 
-      build.onLoad({ filter: /\.html$/ }, async ({ path }) => {
-        const html = await Bun.file(path).text()
-        const { css } = await generator.generate(html)
-        const contents = html.replace('</head>', `<style>${css}</style>\n</head>`)
-
-        return {
-          contents,
-          loader: 'html',
-        }
-      })
-    },
-  }
+    build.onLoad({ filter: /\.html$/ }, async ({ path }) => {
+      const html = await Bun.file(path).text()
+      const { css } = await generator.generate(html)
+      const contents = html.replace('</head>', `<style>${css}</style>\n</head>`)
+      return {
+        contents,
+        loader: 'html',
+      }
+    })
+  },
 }
 
 export default plugin
